6f368c8524951ed41ae861ae3c438a1f08624e74
Keith Ma <keithchezzy@gmail.com>
2022-11-07 10:06:16 -0800

    commit tree 500a06c8a52defa6aa118d68e49bbf5fcd8806e7

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+// function findMatching(name, sought) {
+//     return name.filter(
+//       (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+//     );
+//   }
+// console.log(findMatching);
+
+// function fuzzyMatch(source, testString) {
+//     return source.filter(
+//       (possibleMatch) =>
+//         possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     );
+//   }
+  
+    
+//   function matchName(object, names){
+//     return object.filter((record)=>record.name ===names);
+//   }
+
+function findMatching(name, sought){
+  return name.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+  );
+}
+
+function fuzzyMatch(source, testString){
+  return source.filter(
+    (possibleMatch) =>
+    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(object, names){
+  return object.filter((record)=>record.name ===names);
+}
+
